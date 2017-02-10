ffb69afc50d9a6690491f42da0487feac694750e
Sven <SvePu@users.noreply.github.com>
2017-02-10 18:32:58 +0100

    Update pagemanager.lang.php

--- UPLOAD/inc/languages/english/pagemanager.lang.php
+++ UPLOAD/inc/languages/english/pagemanager.lang.php
@@ -2,8 +2,8 @@
 /*
 	Language file for MyBB-PageManager plugin for MyBB 1.8
 	Language: english
-	Copyright © 2017 Svepu
-	Last change: 2017-02-085
+	Copyright Â© 2017 Svepu
+	Last change: 2017-02-10
 */
 
 $l['pagemanager_page_disabled_redirect']='Sorry, this page is temporary disabled. - You will be redirected to Index page.';
